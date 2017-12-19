5ea35ad91d7ec367cad7d9c39d1edb3650e84d9d
Julian Nymark <julian@tiseit.com>
2017-12-19 17:09:42 +0100

    initial commit (partways through tictactoe example, working)
    now here https://reactjs.org/tutorial/tutorial.html#lifting-state-up

---
+++ src/index.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.css';
+
+interface Props {
+    value: string;
+}
+
+interface State {
+    value: string | null;
+}
+
+class Square extends React.Component<Props, State> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+    onClick = () => {
+        this.setState(
+            { value: 'X' }
+        );
+    }
+    render() {
+        return (
+            <button className="square" onClick={this.onClick}>
+                {this.state.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
